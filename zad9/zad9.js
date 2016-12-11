6bbb95d313995371cdf91150e76b0d3041e302e9
bodzan <bodzan@tlen.pl>
2016-12-11 18:19:00 +0100

    init

---
+++ zad9/zad9.js
@@ -0,0 +1,15 @@
+// Napisz klasę Calculator t.że:
+
+// new Calculator()
+//     .getValue() // 0.
+
+// new Calculator()
+//     .add(40)
+//     .subtract(20)
+//     .getValue() // 20
+
+// new Calculator()
+//     .add(10)
+//     .square()
+//     .subtract(200)
+//     .sqrt() // Error: 'You cannot square negative values'
